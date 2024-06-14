73c8ca76311df32069a2e5a48ec691b864e94d21
Moaz Ashraf <moaazashraf98@gmail.com>
2024-06-14 22:25:26 +0300

    more responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,14 @@ function Header() {
   return (
     <main className={styles.header_wrapper}>
       <div className={styles.container}>
-        <div className={styles.heading}>
+        <header className={styles.heading}>
           <span>turning ideas into</span>
           <br />
           <span>real life </span>
           <span className={styles.heading_gradient}>products</span>
           <br />
           <span>is my calling.</span>
-        </div>
+        </header>
       </div>
     </main>
   );
