3a03d23a33cd9148ee4a2cf79bb4fbb7357f0d27
Moaz Ashraf <moaazashraf98@gmail.com>
2024-07-12 00:53:50 +0300

    add speedINsights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/index.scss";
 import type { AppProps } from "next/app";
 import localFont from "next/font/local";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const dDinCondensed = localFont({
   src: "./D-DINCondensed.woff2",
@@ -23,6 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
       className={`${dDinCondensed.variable} ${markPro.variable} ${circular.variable}`}
     >
       <Component {...pageProps} />
+      <SpeedInsights />
     </main>
   );
 }
